49e564f87ce78a593df872031bf1b60c35cbaed3
iver56 <iver56@hotmail.com>
2015-03-04 12:40:02 +0100

    Feature: report room availability

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
@@ -14,4 +14,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- room/views.py
+++ room/views.py
@@ -1,6 +1,11 @@
 from .models import Room
 from rest_framework import viewsets
 from .serializers import RoomDetailSerializer, RoomListSerializer
+from rest_framework.decorators import detail_route
+from django.core.exceptions import PermissionDenied
+from rest_framework.exceptions import ParseError
+from rest_framework.response import Response
+from django.utils import timezone
 
 
 class RoomViewSet(viewsets.ModelViewSet):
@@ -10,3 +15,22 @@ class RoomViewSet(viewsets.ModelViewSet):
         if self.action == 'retrieve':
             return RoomDetailSerializer
         return RoomListSerializer
+
+    @detail_route(methods=['post'])
+    def report_availability(self, request, pk=None):
+        room = self.get_object()
+        try:
+            is_available = int(request.DATA['is_available'])
+        except:
+            raise ParseError(detail="Must specify is_available that is an integer")
+
+        if is_available < 0:
+            raise ParseError(detail="is_available must be non-negative")
+
+        if is_available:
+            room.available_since = timezone.now()
+        else:
+            room.available_since = None
+        room.save()
+
+        return Response({'status': 'ok'})
